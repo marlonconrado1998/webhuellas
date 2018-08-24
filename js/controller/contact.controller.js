e05e442fc621c2e0ba50c2f40383c119f83a7dae
elconrado <marlon.conrado@zabud.co>
2018-08-23 23:48:10 -0500

    Confirmacion con alert de envio de correo en modulo contacto

--- js/controller/contact.controller.js
+++ js/controller/contact.controller.js
@@ -1,22 +1,30 @@
 app.controller('contactController', contactController)
 
 /** @ngInject */
-contactController.$inject = ['principalService', '$location'];
+contactController.$inject = ['principalService', 'swetService'];
 
-function contactController(principalService) {
+function contactController(principalService, swetService) {
 
     var contact = this;
     contact.loadingMsg = false;
 
     contact.onSendMensaje = function () {
-        contact.loadingMsg = true;
-        principalService.sendMensaje(contact.msg).then(function(resp){
-            if (resp) {
-                contact.loadingMsg = false;
-                toaster("success", "Mensaje enviado correctamente", 5000);
-            }
-        }).catch(function (error) {
-            toaster("warning", "Error al enviar el mensaje", 5000);
+        swetService.SWAL_OPTION(function (response) {
+            if (!response) return false;
+            swetService.SWAL_SUCCESS("Mensaje enviado correctamente.");
+
+        }, function (response) {
+            // contact.loadingMsg = true;
+            return principalService.sendMensaje(contact.msg).then(function (resp) {
+                if (resp) {
+                    return true;
+                    // contact.loadingMsg = false;
+                    // toaster("success", "Mensaje enviado correctamente", 5000);
+                }
+            }).catch(function (error) {
+                swal.showValidationError("Error al enviar el mensaje.");
+                // toaster("warning", "Error al enviar el mensaje", 5000);
+            });
         });
     }
 
